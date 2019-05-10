b66eba670349bd5fece8bcbf05760df155dce67e
ShrMus <shrmus@163.com>
2019-05-11 00:38:51 +0800

    上传一些笔记

---
+++ 知识库/Apache Ant/demo20190330/src/cn/shrmus/app/Main.java
@@ -0,0 +1,13 @@
+package cn.shrmus.app;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("SS");
+		
+		ShrBean shrBean = new ShrBean();
+		shrBean.print();
+		
+		ThreadLocal.withInitial(supplier)
+		
+	}
+}
---
+++ 知识库/Apache Ant/demo20190330/src/cn/shrmus/demo/Demo.java
@@ -0,0 +1,15 @@
+package cn.shrmus.demo;
+
+public class Demo {
+	public static void main(String[] args) {
+		String a = "2";
+		switch (a) {
+		case "2":
+			System.out.println(1);
+			break;
+		default:
+			System.out.println("default");
+			break;
+		}
+	}
+}
